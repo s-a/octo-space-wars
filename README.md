dceb7b2b9557a75e71f73f53cebfe3346fe32b8e
s-a <stephan.ahlf@googlemail.com>
2014-01-11 12:59:49 +0100

    gh-pages sync

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-octo-space-wars
+octoSpaceWars
 ===============
 
-A Web GL Octo "spaceWarSimulation"
+A Web GL Octo "spaceWarSimulation" experiment
+
+http://s-a.github.io/octo-space-wars/
---
+++ refresh-gh-pages.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git checkout gh-pages // go to the gh-pages branch
+git rebase master // bring gh-pages up to date with master
+git push origin gh-pages // commit the changes
+git checkout master // return to the master branch
